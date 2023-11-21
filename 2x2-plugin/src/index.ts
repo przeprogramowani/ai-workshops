bf90c6b12afdc06e61d8b2a85d75fa5658a195d1
psmyrdek <przemek.smyrdek@gmail.com>
2023-11-21 13:01:53 +0100

    fix: update typescript

---
+++ 2x2-plugin/src/index.ts
@@ -0,0 +1,39 @@
+import "dotenv/config";
+
+import OpenAI from 'openai';
+
+import { SYSTEM_MESSAGE, CODE_SNIPPET } from "./prompts";
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY
+});
+
+// createLoader("Pobieram odpowiedź z GPT-4");
+
+const stream = await openai.chat.completions.create({
+  model: "gpt-4-1106-preview",
+  stream: true,
+  messages: [
+    { role: "system", content: SYSTEM_MESSAGE },
+    {
+      role: "user",
+      content: `
+        Przeprowadź Code Review następującego fragmentu kodu:
+
+        ${CODE_SNIPPET}
+
+        Oceń go wg kryteriów złożoności, czytelności oraz reużywalności.
+
+        Do każdego kryterium dodaj ocenę od 1 do 10, gdzie 10 to maksymalna oczekiwana wartość.
+      `,
+    },
+  ]
+});
+
+for await (const chunk of stream) {
+  process.stdout.write(chunk.choices[0]?.delta?.content || '');
+}
+
+// cancelLoader();
+
+// console.log(completion.data.choices[0].message.content);
