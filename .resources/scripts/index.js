cdaf98672a0978e12aaa1e9f321dbca897406a5a
psmyrdek <przemek.smyrdek@gmail.com>
2023-12-06 16:36:20 +0100

    feat: test prompt packing

--- .resources/scripts/index.js
+++ .resources/scripts/index.js
@@ -1,1 +1,23 @@
-console.log('Hello world')
+import fs from "fs";
+import { glob } from "glob";
+
+const files = await glob("../../1x1-prompts/**/*.md");
+
+const prompts = {
+  pl: [],
+  en: [],
+};
+
+files.forEach((pathToFile) => {
+  const content = fs.readFileSync(pathToFile, { encoding: "utf-8" });
+
+  if (pathToFile.includes("prompts/pl")) {
+    prompts.pl.push(content);
+  } else {
+    prompts.en.push(content);
+  }
+});
+
+console.log(prompts);
+
+fs.writeFileSync('./gh-pages/prompts.json', JSON.stringify(prompts, null, 2), 'utf-8');
