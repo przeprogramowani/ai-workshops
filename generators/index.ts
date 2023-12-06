3f35414190492ce7cb51939f4facdaba5c493be2
“mkczarkowski” <marcinczarkowski@onet.eu>
2023-12-06 16:24:05 +0100

    new data

--- generators/index.ts
+++ generators/index.ts
@@ -6,8 +6,9 @@ const openai = new OpenAI({
   apiKey: process.env.OPENAI_PLATFORM_KEY,
 });
 
-console.log('Tworzę plik json z komentarzami użytkowników gier online...');
 
+for (let i = 0; i < 4; i++) {
+console.log(`${i+1}: Tworzę plik json z komentarzami użytkowników gier online...`);
 const comments = await openai.chat.completions.create({
   model: 'gpt-4-1106-preview',
   temperature: 0.8,
@@ -16,38 +17,51 @@ const comments = await openai.chat.completions.create({
     {
         role: 'user',
         content: `
-            Wygeneruj plik json, który zawiera 25 komentarzy użytkownikow gier online w stylu kasyno.
+        Wygeneruj plik json zawierający 25 komentarzy użytkowników gry online w stylu kasyna o nazwie Huuuge Casino.
 
-            W grze można rozgrywać pojedynki z innymi graczami, a także z komputerem.
-            Potencjalne gry to np. sloty, ruletka, poker, black jack, szachy, warcaby, monopoly, scrabble, kółko i krzyżyk, itp.
-            
-            Komentarze powinny być w języku polskim, a ich tematyka powinna dotyczyć jednej z trzech kategorii:
-            - poziomu zaangażowania użytkowników
-            - interfejsu i zrozumienia reguł gry
-            - personalizacji doświadczeń i możliwości dostosowania gry do siebie
+        Opis gry:
+        "Witamy w ekscytującym świecie Huuuge Slots - miejscu docelowym gier społecznościowych i aplikacji kasyna online z najlepszym wyborem 777 slotów, automatów z jackpotami i wielu innych! Wkrocz do świata gier kasynowych!
 
-            Spraw, aby komentarze były neutralne lub krytyczne i nie powtarzały się.
+        Doświadcz blasku gier kasynowych Las Vegas na wyciągnięcie ręki i dołącz do grona szczęśliwych entuzjastów automatów w kasynie online Huuuge 2023. Najlepsze klasyczne gry na automatach są tuż za rogiem! Ciesz się wspaniałym wyborem naszych gier slotowych, takich jak Bars and Sevens 777, Classic Diamonds, 777 of Fortune, Classit Fruits, Triple Chance Jackpots 777 i wiele innych".
 
-            Komentarze powinny być udzielane w okresie od października do grudnia 2023.
-            Komentarze są grupowane wg jednej z trzech kategorii - interfejs_rozgrywka, poradniki_support, problemy_techniczne
+        W Huuuge Casino można rozgrywać pojedynki z innymi graczami, a także z komputerem.
 
-            Powinien być widoczny spadek ocen wraz z upływem czasu.
+        6 listopada pojawiła się ważna aktualizacja: Huuuge Casino 2.0. Po trzech kwartałach cała platforma została przepisana z C++ i Java na Rust, co doprowadziło do znacznie lepszej wydajności i mniejszej liczby błędów. Jednak nie wszystkie funkcje były dostępne w nowej wersji po wydaniu, ponieważ są one nadal rozwijane i planowane na 2024 rok. Najważniejsze brakujące kategorie funkcji to:
+        1. **Engagement** - Mało wciągająca rozgrywka / Mała liczba gier / Mała różnorodność gier.
+        2. **Wellbeing** - Brak kontroli wydatków, popadanie w uzależnienie, frustrujący interfejs, dźwięki i powiadomienia.
+        3 **Social** - Brak możliwości grania ze znajomymi lub poznawania innych graczy.
 
-            Chciałbym, aby każdy komentarz miał takie pola:
-            - data_dodania
-            - autor (fikcyjne imie i nazwisko lub fallback jako "anonim")
-            - komentarz
-            - ocena (od 1 do 5 gwiazdek)
-            - reputacja_autora (od 1 do 5 gwiazdek)
-            - kategoria - "interfejs_rozgrywka" lub "poradniki_support" lub "problemy_techniczne"
-            - platforma - mobile_ios, mobile_android, web
+        Komentarze powinny być w języku polskim. Chciałbym, aby każdy komentarz miał takie pola:
+        - data_dodania
+        - autor (fikcyjna nazwa lub awaryjnie "anonimowy")
+        - komentarz
+        - ocena (od 1 do 10 gwiazdek)
+        - autor_reputacja (od 1 do 5 gwiazdek)
+        - kategoria
+        - platforma - mobile_ios, mobile_android, web
 
-            Zwróć odpowiedź w formacie json w tablicy "komentarze"
-        
+        Komentarze powinny mieć datę dodania między 6 października 2023 r. a 6 grudniem 2023 r.
+
+        6 października 2023 i 5 listopada następującego sentymentu:
+        1. negatywne = 20% komentarzy wspominających o wydajności i losowych błędach
+        2. neutralny = 20% komentarzy
+        3. pozytywne = 60% komentarzy
+
+        6 listopada 2023 r. (po premierze) i 6 grudnia następującego sentymentu:
+        1. negatywne = 60% komentarzy wspominających o problemach w zaangażowaniu, dobrym samopoczuciu i rozrywce społecznościowej
+        2. neutralne = 20% komentarzy
+        3. pozytywny = 20% komentarzy wspominających o poprawie wydajności, stabilności platformy i braku przypadkowych błędów
+
+        Dla każdego sentymentu zakres ocen powinien być następujący:
+        1. negatywny - ocena od 1 do 4
+        2. neutralny - ocena od 5 do 7
+        3. pozytywny - ocena od 8 do 10
+
+        Po 6 listopada 2023 r. powinien być widoczny wyraźny spadek ocen i sentymentu w komentarzach. Komentarzy powinno być co najmniej 25 a najlepiej ponad 50.
         `
     }
   ],
 });
 
 fs.writeFileSync(`data-new-${new Date().getTime()}.json`, comments!.choices[0]!.message!.content!, 'utf8');
-
+}
